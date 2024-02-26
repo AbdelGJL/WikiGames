5068e0280095049d0e6b55271f16631217c1bbea
Mélanie <melxhern@gmail.com>
2024-02-26 23:52:40 +0100

    ajout page jeux

---
+++ app/src/main/java/com/example/wikigames/GameInfo.kt
@@ -0,0 +1,11 @@
+package com.example.wikigames
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GameInfo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_info)
+    }
+}
