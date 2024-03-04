cd92b466d845fa58902fe130b9ee375ba59a4028
Mélanie <melxhern@gmail.com>
2024-03-04 20:43:07 +0100

    pages visu finies

--- app/src/main/java/com/example/wikigames/Profile.kt
+++ app/src/main/java/com/example/wikigames/Profile.kt
@@ -1,11 +1,17 @@
 package com.example.wikigames
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 
 class Profile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
     }
+
+
+
 }
