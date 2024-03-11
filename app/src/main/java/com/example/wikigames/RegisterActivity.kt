efb99519d95a93b6472529d07d3c3ef3505f945f
hematome02 <emma94.fromager@gmail.com>
2024-03-11 22:42:03 +0100

    firebase fonctionnel
    Lorsqu'on Register un utilisateur est crée sur Firebase ainsi qu'une base de données utile plus tard pour les favoris et la bio.
    On ne peut pas créer un compte avec un email déjà présent dans la base de données, plusieurs tests effectués pour vérifier le fonctionnement.

--- app/src/main/java/com/example/wikigames/RegisterActivity.kt
+++ app/src/main/java/com/example/wikigames/RegisterActivity.kt
@@ -27,6 +27,7 @@ class RegisterActivity : AppCompatActivity() {
 }*/
 package com.example.wikigames
 
+
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
@@ -35,6 +36,10 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+
 class RegisterActivity : AppCompatActivity() {
 
     private lateinit var firebaseAuth: FirebaseAuth
@@ -42,6 +47,10 @@ class RegisterActivity : AppCompatActivity() {
     private lateinit var usernameEditText: EditText
     private lateinit var emailEditText: EditText
     private lateinit var passwordEditText: EditText
+
+    private var db= Firebase.firestore
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -60,12 +69,32 @@ class RegisterActivity : AppCompatActivity() {
         val username = usernameEditText.text.toString()
         val email = emailEditText.text.toString()
         val password = passwordEditText.text.toString()
+        val bio = ""
+        val favori: MutableList<String>? = null // mutableListOf("élément1", "élément2", "élément3")
 
         if (email.isNotEmpty() && password.isNotEmpty() && username.isNotEmpty()) {
             // Firebase authentication for user registration
             firebaseAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this) { task ->
                     if (task.isSuccessful) {
+
+
+
+                        //modification debut
+                        val userMap= hashMapOf(
+                            "username" to username,
+                            "mail" to email,
+                            "password" to password,
+                            "biography" to bio,
+                            "favori" to favori
+                        )
+
+                        val userId = firebaseAuth.currentUser!!.uid
+
+                        db.collection("user").document(userId).set(userMap)
+
+
+// modification fin
                         // Registration successful, navigate to the main activity or any other desired activity
                         val intent = Intent(this, MainActivity::class.java)
                         Toast.makeText(
@@ -86,6 +115,7 @@ class RegisterActivity : AppCompatActivity() {
             Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_SHORT).show()
         }
     }
+
 
     // Function to navigate back to the main activity
     fun onReverse(view: View) {
